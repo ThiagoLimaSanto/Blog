b41ad58dc8e9cb8f623e6bb7a75b8c937e48d3e2
Thiago Lima <tlima7601@gmail.com>
2026-02-11 10:38:27 -0300

    titulo dinamico

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,14 +1,26 @@
 import { findPostBySlugCached } from "@/lib/Post/queries";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
     params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({
+    params,
+}: PostSlugPageProps): Promise<Metadata> {
+    const { slug } = await params;
+    const post = await findPostBySlugCached(slug);
+
+    return {
+        title: post.title,
+        description: post.excerpt,
+    };
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
     const { slug } = await params;
 
     const post = await findPostBySlugCached(slug);
-
 
     return <h1 className="text-7xl font-extrabold py-16">{post.content}</h1>;
 }
