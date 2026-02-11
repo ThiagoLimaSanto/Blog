ba52c073776cf5389a0936085ec99209b69705b6
Thiago Lima <tlima7601@gmail.com>
2026-02-11 11:48:15 -0300

    criando o componente para uma pagina de erro

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,14 @@
+import ErrorMessage from "@/components/ErrorMessage";
 import clsx from "clsx";
 
 export default function NotFoundPage() {
     return (
         <>
-        <title>404 Not Found</title>
-            <div
-                className={clsx(
-                    "min-h-80",
-                    "bg-slate-900",
-                    "text-slate-100",
-                    "mb-16",
-                    "p-8",
-                    "rounded-xl",
-                    "flex",
-                    "items-center",
-                    "justify-center",
-                    "text-center",
-                )}
-            >
-                <div>
-                    <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-                    <p>Erro 404 - A página que vocé procurou não foi encontrada.</p>
-                </div>
-            </div>
+            <ErrorMessage
+                pageTitle="404 - Not Found"
+                contentTitle="404"
+                content="The page you are looking for does not exist."
+            />
         </>
     );
 }
